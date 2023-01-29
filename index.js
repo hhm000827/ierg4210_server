9566c88e167a3386acaad5a9a980625f3728ebf7
hhm000827 <hauhoman@yahoo.com.hk>
2023-01-30 01:07:33 +0800

    serve image from folder

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 var cors = require("cors");
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + "/config.env" });
 
 const app = express();
 app.use(cors());
+app.use("/images", express.static("image"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
