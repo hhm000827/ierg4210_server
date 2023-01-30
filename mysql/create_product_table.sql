7b88a4ba04dde004dee47471ae7b6c64a12e8a09
hhm000827 <hauhoman@yahoo.com.hk>
2023-01-31 00:15:43 +0800

    tweak for create_product_table.sql

--- mysql/create_product_table.sql
+++ mysql/create_product_table.sql
@@ -6,8 +6,10 @@ CREATE TABLE PRODUCTS (
  img varchar(255) NOT NULL,
  inventory int NOT NULL,
  description varchar(255) NOT NULL,
- FOREIGN KEY(cid) REFERENCES CATEGORIES(cid) on delete cascade
+ FOREIGN KEY(cid) REFERENCES CATEGORIES(cid) on delete cascade on update cascade
 );
+
+CREATE INDEX I1 ON PRODUCTS(cid);
 
 insert into cart.PRODUCTS values (1,2,"saber",12,"1.jpg",3,"saber");
 insert into cart.PRODUCTS values (2,2,"caster",15,"2.jpg",5,"caster");
@@ -18,5 +20,3 @@ insert into cart.PRODUCTS values (6,3,"wa2000",20,"6.jpg",2,"wa2000");
 insert into cart.PRODUCTS values (7,1,"bagpipe",20,"7.jpg",2,"bagpipe");
 insert into cart.PRODUCTS values (8,1,"skadi",13,"8.jpg",5,"skadi");
 insert into cart.PRODUCTS values (9,1,"mudrock",21,"9.jpg",4,"mudrock");
-
-CREATE INDEX I1 ON PRODUCTS(cid);
