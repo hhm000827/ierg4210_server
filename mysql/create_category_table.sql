e477af4608a097e3971637667bf8bab7cfdf66df
hhm000827 <hauhoman@yahoo.com.hk>
2023-01-30 17:24:04 +0800

    complete setting sql structure

--- mysql/create_category_table.sql
+++ mysql/create_category_table.sql
@@ -2,3 +2,7 @@ CREATE TABLE CATEGORIES (
  cid int PRIMARY KEY,
  name varchar(255) NOT NULL,
 );
+
+insert into cart.CATEGORIES values (1,"Arknight");
+insert into cart.CATEGORIES values (2,"Fate Grand Order");
+insert into cart.CATEGORIES values (3,"Girl Frontline");
--- mysql/create_product_table.sql
+++ mysql/create_product_table.sql
@@ -8,3 +8,15 @@ CREATE TABLE PRODUCTS (
  description varchar(255) NOT NULL,
  FOREIGN KEY(cid) REFERENCES CATEGORIES(cid) on delete cascade
 );
+
+insert into cart.PRODUCTS values (1,2,"saber",12,"1.jpg",3,"saber");
+insert into cart.PRODUCTS values (2,2,"caster",15,"2.jpg",5,"caster");
+insert into cart.PRODUCTS values (3,2,"foreigner",20,"3.jpg",3,"foreigner");
+insert into cart.PRODUCTS values (4,3,"iws2000",20,"4.jpg",1,"iws2000");
+insert into cart.PRODUCTS values (5,3,"kar98k",14,"5.jpg",4,"kar98k");
+insert into cart.PRODUCTS values (6,3,"wa2000",20,"6.jpg",2,"wa2000");
+insert into cart.PRODUCTS values (7,1,"bagpipe",20,"7.jpg",2,"bagpipe");
+insert into cart.PRODUCTS values (8,1,"skadi",13,"8.jpg",5,"skadi");
+insert into cart.PRODUCTS values (9,1,"mudrock",21,"9.jpg",4,"mudrock");
+
+CREATE INDEX I1 ON PRODUCTS(cid);
