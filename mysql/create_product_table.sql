740e777270fd5956d3880ac429d3684668a01a31
hhm000827 <hauhoman@yahoo.com.hk>
2023-02-15 16:47:19 +0800

    change mysql password and table info

--- mysql/create_product_table.sql
+++ mysql/create_product_table.sql
@@ -14,7 +14,7 @@ CREATE INDEX I1 ON PRODUCTS(cid);
 insert into cart.PRODUCTS values (1,2,"saber",12,"1.jpg",3,"saber");
 insert into cart.PRODUCTS values (2,2,"caster",15,"2.jpg",5,"caster");
 insert into cart.PRODUCTS values (3,2,"foreigner",20,"3.jpg",3,"foreigner");
-insert into cart.PRODUCTS values (4,3,"iws2000",20,"4.jpg",1,"iws2000");
+insert into cart.PRODUCTS values (4,3,"iws2000",20,"iws2000.jpeg",1,"iws2000");
 insert into cart.PRODUCTS values (5,3,"kar98k",14,"5.jpg",4,"kar98k");
 insert into cart.PRODUCTS values (6,3,"wa2000",20,"6.jpg",2,"wa2000");
 insert into cart.PRODUCTS values (7,1,"bagpipe",20,"7.jpg",2,"bagpipe");
